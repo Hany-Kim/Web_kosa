4bee49f81bf5e2909901a27ce5b0c198a0a2cdfb
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2024-04-11 14:03:57 +0900

    if example

---
+++ DAY_24_04_11/first.js
@@ -0,0 +1,12 @@
+function hello(name) {
+    console.log("Hello~" + name);
+    var a = 10;
+    b = 20;
+  }
+
+  hello("KilDong");
+//   console.log(a); // 에러나니까 주석, 에러나면 이후에 실행안함.
+  console.log(b);
+
+  var contentEl = document.querySelector("#content");
+  console.log(contentEl);
